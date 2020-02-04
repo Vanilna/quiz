b580116eb8ffb7661fcd362dd148b07ddeb6db53
Vanilna <lesyalp@gmail.com>
2020-02-04 20:16:31 +0100

    clear App component, add logo and initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo.png";
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.App}>
+      <header className={classes.Logo}>
+        <img src={logo} alt="Quiz time" />
       </header>
+
+      <p className={classes.Reference}>
+        Logo created by Freshcare from{" "}
+        <a className={classes.Reference__link} href="https://pngtree.com/">
+          Pngtree.com
+        </a>
+      </p>
     </div>
   );
 }
